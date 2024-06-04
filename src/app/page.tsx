f7077b7a06fc246b9c1ea822e47b0b5ac97cc4ac
hzjiangren <hzjiangren@corp.netease.com>
2024-06-04 10:11:37 +0800

    feat: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ import Beian from '@/components/beian';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.center}>
+      <div className={styles.text}>
         <p style={{
           fontSize: "30px",
         }}>橙堡之家</p>
       </div>
-      <img style={{width: '400px'}} alt="橙堡" width="400" src="https://i.postimg.cc/qMqg29SV/pngg.jpg" />
-      <div className={styles.center}>
+      <img className={styles.showimg} alt="橙堡" src="https://i.postimg.cc/qMqg29SV/pngg.jpg" />
+      <div className={styles.text}>
         <p style={{
           fontSize: "24px",
         }}>敬请期待...</p>
       </div>
-      <Beian type="fixed" />
+      <Beian />
     </main>
   );
 }
--- src/components/beian/index.js
+++ src/components/beian/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './index.module.css';
 
-export default function Beian({ type }) {
+export default function Beian({ type = '' }) {
     return (
         <div className={`${styles.wrap} ${type === 'fixed' ? styles.fixed : ''}`} >
             <a href="https://beian.miit.gov.cn" target="_blank">浙ICP备2021040693号-2</a>
